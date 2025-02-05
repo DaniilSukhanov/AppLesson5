c267ba96b585932935423f5da5dc92ee2abe6694
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2025-02-05 18:14:28 +0300

    Initial Commit

---
+++ AppLesson5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLesson5
+//
+//  Created by Даниил Суханов on 05.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
