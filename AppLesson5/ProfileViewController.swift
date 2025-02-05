66ee80524ee2a6d937baad59543bc64eefbf49e3
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2025-02-05 18:20:46 +0300

    init

--- AppLesson5/ProfileViewController.swift
+++ AppLesson5/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  AppLesson5
 //
 //  Created by Даниил Суханов on 05.02.2025.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
