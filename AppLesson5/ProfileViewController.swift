ac30eeda9b8b237363260d43863c46cc08eea6a9
DaniilSukhanov <82699248+DaniilSukhanov@users.noreply.github.com>
2025-02-05 20:26:55 +0300

    add CollectionView

--- AppLesson5/ProfileViewController.swift
+++ AppLesson5/ProfileViewController.swift
@@ -33,10 +33,26 @@ private enum UIConstants {
     static let cardViewHeight: CGFloat = 200
 }
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     private var cardBottomConstraint: NSLayoutConstraint!
     private var isCardCollapsed = true
+    private var collectionViewTopConstraint: NSLayoutConstraint!
+    
+    private let achievements = Achievement.demoData
+    
+    private let collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.itemSize = UIConstants.collectionItemSize
+        layout.minimumInteritemSpacing = UIConstants.collectionMinimumInteritemSpacing
+        layout.minimumLineSpacing = UIConstants.collectionMinimumLineSpacing
+        layout.sectionInset = UIConstants.collectionSectionInset
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.backgroundColor = .clear
+        view.showsHorizontalScrollIndicator = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     private let cardView: UIView = {
         let view = UIView()
@@ -86,6 +102,7 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .cyan
         setupUI()
+        setupCollectionView()
     }
     
     private func setupUI() {
@@ -97,6 +114,7 @@ class ProfileViewController: UIViewController {
         
         cardView.addSubview(profileStackView)
         view.addSubview(cardView)
+        view.addSubview(collectionView)
         
         cardView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handlerCardTop)))
         
@@ -104,6 +122,7 @@ class ProfileViewController: UIViewController {
         let stackViewBottomPadding: CGFloat = 16
         let initialCardPosition = -(nameLabelHeight + stackViewBottomPadding + view.safeAreaInsets.bottom)
         cardBottomConstraint = cardView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: initialCardPosition)
+        collectionViewTopConstraint = collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: UIConstants.cardViewHeight)
         
         NSLayoutConstraint.activate([
             cardBottomConstraint,
@@ -117,14 +136,26 @@ class ProfileViewController: UIViewController {
             profileStackView.centerYAnchor.constraint(equalTo: cardView.centerYAnchor),
             
             avatarImageView.widthAnchor.constraint(equalToConstant: UIConstants.avatarImageSize),
-            avatarImageView.heightAnchor.constraint(equalToConstant: UIConstants.avatarImageSize)
+            avatarImageView.heightAnchor.constraint(equalToConstant: UIConstants.avatarImageSize),
             
+            collectionViewTopConstraint,
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+        
+        cardView.layer.zPosition = 1
+    }
+    
+    private func setupCollectionView() {
+        collectionView.register(AchievementCell.self, forCellWithReuseIdentifier: "AchievementCell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.alpha = UIConstants.collectionHiddenAlpha
     }
     
     @objc func handlerCardTop() {
         isCardCollapsed.toggle()
-        
         UIView.animate(withDuration: 0.6, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: .curveEaseInOut) {
             if self.isCardCollapsed {
                 let nameLabelHeight = self.nameLabel.intrinsicContentSize.height
@@ -135,12 +166,25 @@ class ProfileViewController: UIViewController {
                 self.cardBottomConstraint.constant = -(self.view.frame.height - self.view.safeAreaInsets.top - UIConstants.cardViewHeight)
             }
             
+            self.collectionView.transform = self.isCardCollapsed ? CGAffineTransform(translationX: 0, y: self.view.frame.height / 2) : .identity
+            self.collectionView.alpha = self.isCardCollapsed ? 0 : 1
             self.boiLabel.alpha = self.isCardCollapsed ? 0 : 1
             
             self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.collectionView.isUserInteractionEnabled = !self.isCardCollapsed
         }
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return achievements.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AchievementCell", for: indexPath) as! AchievementCell
+        cell.configure(with: achievements[indexPath.item])
+        return cell
+    }
 
 }
 
